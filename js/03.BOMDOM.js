34710a809816d508b20fc0f7a1986f6b24896747
booldook <booldook@gmail.com>
2021-03-31 14:38:26 +0900

    ...

---
+++ js/03.BOMDOM.js
@@ -0,0 +1,8 @@
+console.log(screen.width);
+console.log(screen.height);
+console.log(screen.availWidth);
+console.log(screen.availHeight);
+console.log(screen.colorDepth);
+console.log(screen.pixelDepth);
+
+// location.href = '//daum.net';
