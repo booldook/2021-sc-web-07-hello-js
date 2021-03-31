2ff8644d9ad38dc222882eaf5d218403c077f401
booldook <booldook@gmail.com>
2021-03-31 15:03:47 +0900

    ...

--- js/03.BOMDOM.js
+++ js/03.BOMDOM.js
@@ -1,3 +1,4 @@
+// 아래의 내용은 '이런게 있구나~' 참조만 하세요.
 console.log(screen.width);
 console.log(screen.height);
 console.log(screen.availWidth);
@@ -5,4 +6,13 @@ console.log(screen.availHeight);
 console.log(screen.colorDepth);
 console.log(screen.pixelDepth);
 
-// location.href = '//daum.net';
+console.log(location.hostname);
+console.log(location.pathname);
+console.log(location.protocol);
+console.log(location.port);
+
+console.log(window.clientInformation);
+console.log(navigator.appName);
+console.log(navigator.appCodeName);
+console.log(navigator.platform);
+console.log(navigator.product);
