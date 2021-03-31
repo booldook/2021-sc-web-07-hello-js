0dd74f61b52645aed163f9a8e8537d260aa4dbe7
booldook <booldook@gmail.com>
2021-03-31 15:57:53 +0900

    ...

--- js/03.BOMDOM.js
+++ js/03.BOMDOM.js
@@ -16,3 +16,30 @@ console.log(navigator.appName);
 console.log(navigator.appCodeName);
 console.log(navigator.platform);
 console.log(navigator.product);
+
+
+// 드디어 사용자 정의 함수
+function booldook(query) {
+	// console.log(query);
+	// console.log( alert(query) );			// 리턴값이 없는 함수(void형)
+	// console.log( confirm(query) );		// 리턴값이 있는 함수(String, Number, Boolean ...)
+	if(confirm(query)) {
+		alert('참말!');
+	}
+	else {
+		alert('그짓말!');
+	}
+}
+
+
+function pomulsun(x) {	// 리턴값이 있는 함수
+	alert(x * x);
+	return x * x;
+}
+
+function pomulsun2(x) {	// 리턴값이 없는 함수
+	alert(x * x);
+}
+
+// console.log( pomulsun(5) );		//25
+// console.log( pomulsun2(5) );	//undefined
