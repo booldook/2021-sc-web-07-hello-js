112581b5ff4cc307c7247af8f40eebcebe49f128
booldook <booldook@gmail.com>
2021-04-01 14:09:59 +0900

    ...

---
+++ js/06.fade.js
@@ -0,0 +1,19 @@
+var zIdx = 1;
+
+function show1() {
+	zIdx = zIdx + 1;
+	console.log(zIdx);
+	document.getElementById('banner1').style.zIndex = zIdx;
+}
+
+function show2() {
+	zIdx = zIdx + 1;
+	console.log(zIdx);
+	document.getElementById('banner2').style.zIndex = zIdx;
+}
+
+function show3() {
+	zIdx = zIdx + 1;
+	console.log(zIdx);
+	document.getElementById('banner3').style.zIndex = zIdx;
+}
