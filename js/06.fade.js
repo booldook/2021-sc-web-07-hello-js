497f3a5ebe36642206e633622f41228e08c65c41
booldook <booldook@gmail.com>
2021-04-01 14:48:40 +0900

    v2 완성

--- js/06.fade.js
+++ js/06.fade.js
@@ -1,19 +1,7 @@
 var zIdx = 1;
 
-function show1() {
+function show(id) {
 	zIdx = zIdx + 1;
 	console.log(zIdx);
-	document.getElementById('banner1').style.zIndex = zIdx;
-}
-
-function show2() {
-	zIdx = zIdx + 1;
-	console.log(zIdx);
-	document.getElementById('banner2').style.zIndex = zIdx;
-}
-
-function show3() {
-	zIdx = zIdx + 1;
-	console.log(zIdx);
-	document.getElementById('banner3').style.zIndex = zIdx;
+	document.getElementById(id).style.zIndex = zIdx;
 }
