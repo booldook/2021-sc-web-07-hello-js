1d13229a87576d56a69ef5197fd71fd6388d5bd5
booldook <booldook@gmail.com>
2021-04-01 15:34:00 +0900

    v2 완료

--- js/06.fade.js
+++ js/06.fade.js
@@ -1,7 +1,12 @@
 var zIdx = 1;
 
-function show(id) {
+function show(id, n) {
 	zIdx = zIdx + 1;
-	console.log(zIdx);
 	document.getElementById(id).style.zIndex = zIdx;
+	document.getElementsByClassName('pager')[0].style.color = '#ccc';
+	document.getElementsByClassName('pager')[1].style.color = '#ccc';
+	document.getElementsByClassName('pager')[2].style.color = '#ccc';
+	document.getElementsByClassName('pager')[n].style.color = '#2896f0';
 }
+
+show('banner1', 0);
