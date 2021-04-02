bc92c155a74fc9559e37ebc56bff7ab5c2097003
booldook <booldook@gmail.com>
2021-04-02 11:24:47 +0900

    dom

---
+++ js/09.dom.js
@@ -0,0 +1,14 @@
+/* var document = {
+	html: {
+		head: {
+			innerHTML: '<...>'
+		},
+		body: {
+			innerHTML: '<...>'
+		}
+	}
+} 
+console.log(document.body.innerHTML);
+document.body.innerHTML += '<h1>BOOLDOOK</h1>';
+*/
+
