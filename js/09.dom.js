1c5130cefa1dbfb9e4064ec0e434854a74ff84ea
booldook <booldook@gmail.com>
2021-04-02 11:46:24 +0900

    push

--- js/09.dom.js
+++ js/09.dom.js
@@ -12,3 +12,17 @@ console.log(document.body.innerHTML);
 document.body.innerHTML += '<h1>BOOLDOOK</h1>';
 */
 
+var wrapper = document.getElementById('wrapper');
+var appendTxt = document.getElementById('appendTxt');
+
+function make() {
+	wrapper.innerHTML = wrapper.innerHTML + '<p>'+appendTxt.value+'</p>';
+	appendTxt.value = '';
+	appendTxt.focus();
+}
+
+function remove() {
+	wrapper.innerHTML = '';
+	appendTxt.value = '';
+	appendTxt.focus();
+}
