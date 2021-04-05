9ba1cb688f1e93d49ff0f76e0ea8043f807c49ab
booldook <booldook@gmail.com>
2021-04-05 15:07:34 +0900

    Math에서 꼭 알아야될 함수

--- js/15.box.js
+++ js/15.box.js
@@ -1,3 +1,29 @@
+/************ 선수학습 *************/
+console.log(Math)
+
+var n = 10.11
+console.log( Math.floor(n) )	// 무조건 버림
+console.log( Math.ceil(n) )		// 무조건 올림
+console.log( Math.round(n) )	// 반올림
+
+var n2 = -12.55
+console.log( Math.abs(n2) )		// 절대값
+
+var random = Math.random()		// 0부터 1보다 작은 실수(0 ~ 0.9999999999999999)
+console.log(random)
+
+// console.log( Math.floor(0.829347891241 * 100) )
+// 예 0 ~ 44에서 랜덤한 정수를 구하시오.
+// 공식: Math.floor(Math.random() * 45)
+
+console.log( Math.floor(Math.random() * 45) + 1 )
+console.log( Math.floor(Math.random() * 45) + 1 )
+console.log( Math.floor(Math.random() * 45) + 1 )
+console.log( Math.floor(Math.random() * 45) + 1 )
+console.log( Math.floor(Math.random() * 45) + 1 )
+console.log( Math.floor(Math.random() * 45) + 1 )
+
+
 /************ 전역설정 *************/
 
 
