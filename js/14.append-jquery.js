0168c95799deb6621fb77a180009f551b9bfe397
booldook <booldook@gmail.com>
2021-04-05 14:01:43 +0900

    14.jquery 만드는 중

--- js/14.append-jquery.js
+++ js/14.append-jquery.js
@@ -1,14 +1,24 @@
 /************ 전역설정 *************/
-
+var stage = $('.stage')
+var btAppend = $('.bt-append')
+var btRemove = $('.bt-remove')
 
 /************ 사용자 함수 *************/
 
 
 /************ 이벤트 설정 *************/
+$('.bt-append').on('click', onAppend)
+$('.bt-remove').on('click', onRemove)
 
 
 /************ 이벤트 콜백 *************/
+function onAppend() {
+	$('.stage').append('<div class="box"></div>')
+}
 
+function onRemove() {
+	$('.stage').empty()
+}
 
 
 
