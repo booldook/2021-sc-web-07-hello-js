8bd3a55ae019d610f02484b25beadbb39f1a563d
booldook <booldook@gmail.com>
2021-04-06 13:40:56 +0900

    로또 숫자 뽑기 완료

--- js/16.lotto.js
+++ js/16.lotto.js
@@ -17,8 +17,15 @@ $('#bt-lotto').on('click', onLotto)
 /***************** 이벤트 콜백 *******************/
 function onLotto() {
 	var numbers = [], i, n
-	for(i=0; i<6; i++) {
+	// while(조건): 조건이 참일때까지 반복
+	while(numbers.length < 6) {	
 		n = Math.floor(Math.random() * 45) + 1;
+		if(numbers.indexOf(n) === -1) {
+			numbers.push(n)
+		}
 	}
+	// numbers.sort(function(a, b) { return a - b }) 오름차순
+	// numbers.sort(function(a, b) { return b - a }) 내림차순
+	numbers.sort(function(a, b) { return a - b })
 	console.log(numbers)
 }
