107cd73956cb66cb3c4c34093db54e4bd24932ab
booldook <booldook@gmail.com>
2021-04-07 10:21:04 +0900

    로또 완성

--- js/16.lotto.js
+++ js/16.lotto.js
@@ -11,31 +11,44 @@
 
 /***************** 이벤트 등록 *******************/
 $('#bt-lotto').on('click', onLotto)
+$('#bt-reset').on('click', onReset)
 
 
 
 /***************** 이벤트 콜백 *******************/
+function onReset() {
+	$('.disp-wrapper .display').empty()
+	$('.history-wrapper').empty()
+}
+
+
 function onLotto() {
+	/******* 변수선언 *******/
 	var numbers = [], n, i, color
 	// while(조건): 조건이 참일때까지 반복
+
+	/******* 랜덤한 로또번호 6개를 추출 *******/
 	while(numbers.length < 6) {	
 		n = Math.floor(Math.random() * 45) + 1;
-		if(numbers.indexOf(n) === -1) {
-			numbers.push(n)
-		}
+		if(numbers.indexOf(n) === -1) numbers.push(n)
 	}
+
+	/******* 추출된 번호를 오름차순 정렬 *******/
 	// numbers.sort(function(a, b) { return a - b }) 오름차순
 	// numbers.sort(function(a, b) { return b - a }) 내림차순
 	numbers.sort(function(a, b) { return a - b })
-	// console.log(numbers)
-	$('.display').empty()
+
+	/******* .display에 표현 *******/
+	$('.disp-wrapper .display').empty()
 	for(i=0; i<6; i++) {
 		if(numbers[i] <= 10) { color = 'yellow' }
 		else if(numbers[i] <= 20) { color = 'blue' }
 		else if(numbers[i] <= 30) { color = 'red' }
 		else if(numbers[i] <= 40) { color = 'grey' }
 		else { color = 'green' }
-		// console.log(color)
-		$('.display').append('<li class="ball '+color+'">'+numbers[i]+'</li>');
+		$('.disp-wrapper .display').append('<li class="ball '+color+'">'+numbers[i]+'</li>');
 	}
+
+	/******* .history-wrapper에 넣어주기 *******/
+	$('.history-wrapper').prepend($('.disp-wrapper .display').clone())
 }
