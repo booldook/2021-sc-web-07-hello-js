8cf87675de30ed163517eeef8212666e70c7c991
booldook <booldook@gmail.com>
2021-04-07 14:05:52 +0900

    form validation

--- js/17.score.js
+++ js/17.score.js
@@ -14,10 +14,40 @@ $(document.querySelector('.wrapper'))	=> return jQuery object
 	avg: 85,
 	rank: 2
 } */
+
+/******************* 글로벌 설정 *********************/
 var scores = []
 
 
+/******************* 사용자 함수 *********************/
+
+
+/******************* 이벤트 등록 *********************/
+
+
+/******************* 이벤트 콜백 *********************/
 function onScoreSubmit(f) {
+	// var f = document.scoreForm
+	var name = f.name.value
+	var kor = f.kor.value
+	var eng = f.eng.value
+	var math = f.math.value
+	if(name === '') {
+		alert('이름을 입력하세요.')
+		return false
+	}
+	if(kor === '') {
+		alert('국어점수를 입력하세요.')
+		return false
+	}
+	if(eng === '') {
+		alert('영어점수를 입력하세요.')
+		return false
+	}
+	if(math === '') {
+		alert('수학점수를 입력하세요.')
+		return false
+	}
 	
 	return false
 }
