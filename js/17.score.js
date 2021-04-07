4c41b1d843e2cf4776cdceb0b7e9e1ddd396fc44
booldook <booldook@gmail.com>
2021-04-07 14:59:33 +0900

    아이고 어렵다~

--- js/17.score.js
+++ js/17.score.js
@@ -20,34 +20,89 @@ var scores = []
 
 
 /******************* 사용자 함수 *********************/
-
+function scoreRender() {
+	// 테이블에 점수를 등록
+	var i, html
+	$('.score-tb tbody').empty()
+	for(i=0; i<scores.length; i++) {
+		html  = '<tr>'
+		html += '<td>'+(i+1)+'</td>'
+		html += '<td>'+scores[i].name+'</td>'
+		html += '<td>'+scores[i].kor+'점</td>'
+		html += '<td>'+scores[i].eng+'점</td>'
+		html += '<td>'+scores[i].math+'점</td>'
+		html += '<td>'+scores[i].total+'점</td>'
+		html += '<td>'+scores[i].avg+'점</td>'
+		html += '<td>등</td>'
+		html += '<td>'
+		html += '<button class="bt-change">수정</button>'
+		html += '<button class="bt-remove">삭제</button>'
+		html += '</td>'
+		html += '</tr>'
+		// console.log(html)
+		$('.score-tb tbody').append(html)
+	}
+}
 
 /******************* 이벤트 등록 *********************/
 
 
 /******************* 이벤트 콜백 *********************/
 function onScoreSubmit(f) {
+
+	// 데이터 저장
 	// var f = document.scoreForm
-	var name = f.name.value
-	var kor = f.kor.value
-	var eng = f.eng.value
-	var math = f.math.value
+	var name = f.name.value.trim()
+	var kor = f.kor.value.trim()
+	var eng = f.eng.value.trim()
+	var math = f.math.value.trim()
+	
+	// 데이터 검증
 	if(name === '') {
 		alert('이름을 입력하세요.')
+		f.name.focus()
 		return false
 	}
 	if(kor === '') {
 		alert('국어점수를 입력하세요.')
+		f.kor.focus()
 		return false
 	}
 	if(eng === '') {
 		alert('영어점수를 입력하세요.')
+		f.eng.focus()
 		return false
 	}
 	if(math === '') {
 		alert('수학점수를 입력하세요.')
+		f.math.focus()
 		return false
 	}
 	
+	// 데이터 수정
+	kor = Number(kor)
+	eng = Number(eng)
+	math = Number(math)
+	total = kor + eng + math
+	avg = Number((total/3).toFixed(2))
+	// console.log(kor, eng, math, total, avg)
+
+	// 배열에 데이터를 저장
+	scores.push({
+		name: name,
+		kor: kor,
+		eng: eng,
+		math: math,
+		total: total,
+		avg: avg
+	})
+	console.log(scores)
+
+	// form을 초기화
+	f.reset()
+
+	// 데이터를 화면에 표현한다
+	scoreRender()
+
 	return false
 }
